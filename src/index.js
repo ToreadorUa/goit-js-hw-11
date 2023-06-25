abef55de50ad84b0014bc16931550fc14dee36b1
ToreadorUa <99kava@gmail.com>
2023-06-25 14:28:43 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -3,55 +3,66 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('.search-form');
+const list = document.querySelector('.list');
+
 
 form.addEventListener('submit', onsubmit);
 
 function onsubmit(evt) {
     evt.preventDefault();
-    let query = evt.target.value;
-      console.log(query);
-      // let query =
-      const {
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      } = async () => {};
-    };
+  let  searchQuery= evt.currentTarget.elements.searchQuery.value;
+  
+  getQuery(searchQuery);
+};
+    
+var lightbox = new SimpleLightbox('.gallery a');
+
+
+async function getQuery(q) {
+    //  https://pixabay.com/api/?key=37611793-ff698e652459b51eca316bb1c&q=cats&image_type=photo&orientation=horizontal&safesearch=true
+  const config = { 
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    params: {
+    key: '37611793-ff698e652459b51eca316bb1c',
+    q: q,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: 1,
+    per_page:30,
+    },
+   }
+  
+  await axios.get(`https://pixabay.com/api/`, config )
+    .then(resp => {
+      console.log(createMarkup(resp.data.hits))
+      list.insertAdjacentHTML('afterbegin', createMarkup(resp.data.hits))
+    })
+  .catch(err=>{console.error(err)})
 }
 
-// function getQuery(q) {
-//   // const API_KEY = '37611793-ff698e652459b51eca316bb1c';
-//   //  https://pixabay.com/api/?key=37611793-ff698e652459b51eca316bb1c&q=cats&image_type=photo&orientation=horizontal&safesearch=true
-//   const option = {
-//     key: '37611793-ff698e652459b51eca316bb1c',
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: true,
-//   };
-// }
+function createMarkup(arr) {
+ return  arr.map(({ webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads
+  }) => `<li class="gallery__item" style = "list-style-type: none">
+            <a class="gallery__link" href="${largeImageURL}">
+            <img class="gallery__image" src="${webformatURL}" alt="${tags}" title="${tags}"/>
+            <div class="info">
+              <h2>Likes <br>${likes}</h2>
+              <h2>Views <br>${views}</h2>
+              <h2>Downloads <br>${downloads}</h2>
+              <h2>Comments <br>${comments}</h2>
+            </div>
+            </a>
+                </li>`
+    
+     ).join('');
 
-// {
-//     url: '/user',
-//     baseURL: 'https://pixabay.com/api/',
-//         params: {
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: true,
-//   },
-// }
-
-// q - термин для поиска. То, что будет вводить пользователь.
-// image_type - тип изображения. Мы хотим только фотографии, поэтому задай значение photo.
-// orientation - ориентация фотографии. Задай значение horizontal.
-// safesearch - фильтр по возрасту. Задай значение true.
-// webformatURL - ссылка на маленькое изображение для списка карточек.
-// largeImageURL - ссылка на большое изображение.
-// tags - строка с описанием изображения. Подойдет для атрибута alt.
-// likes - количество лайков.
-// views - количество просмотров.
-// comments - количество комментариев.
-// downloads - количество загрузок.
+}
